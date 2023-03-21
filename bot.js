38b2b1d3fadd147524e6646c4d43916fbd17cb93
krates98 <krates198@gmail.com>
2023-03-21 15:59:27 +0530

    fixing port

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const { Configuration, OpenAIApi } = require("openai");
 
 const botToken = process.env.botToken;
 const openAiToken = process.env.openAiToken;
+const port = process.env.PORT || 3000; // Use the port provided by Heroku or use 3000 as a default
 
 const config = new Configuration({
   apiKey: openAiToken,
@@ -16,37 +17,27 @@ const bot = new TelegramBot(botToken, { polling: true });
 let conversationStarted = false; // flag to keep track of whether the conversation has started or not
 
 bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  const message =
-    "Welcome to Telegram ChatBot. Please choose an action:\n\n" +
-    "/help - Get help\n" +
-    "/menu - Show menu\n" +
-    "/website - Get website URL\n";
-
-  const options = {
-    reply_markup: {
-      keyboard: [
-        [{ text: "/help" }, { text: "/menu" }],
-        [{ text: "/website" }],
-      ],
-      resize_keyboard: true,
-    },
-  };
-
-  bot.sendMessage(chatId, message, options);
+  bot.sendMessage(msg.chat.id, "Welcome to Telegram ChatBot");
   conversationStarted = true; // set the flag to true when the user sends the "/start" command
 });
 
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
-  const messageText = msg.text;
 
-  if (messageText === "/help") {
+  if (!conversationStarted) {
+    bot.sendMessage(
+      chatId,
+      "Please start the conversation by sending the /start command"
+    );
+    return;
+  }
+
+  if (msg.text === "/help") {
     bot.sendMessage(chatId, "This is a help message.");
     return;
   }
 
-  if (messageText === "/menu") {
+  if (msg.text === "/menu") {
     const message =
       "Please choose an action:\n\n" +
       "/help - Get help\n" +
@@ -67,7 +58,7 @@ bot.on("message", async (msg) => {
     return;
   }
 
-  if (messageText === "/website") {
+  if (msg.text === "/website") {
     bot.sendMessage(chatId, "https://www.google.com");
     return;
   }
@@ -76,9 +67,14 @@ bot.on("message", async (msg) => {
   const reply = await openai.createCompletion({
     max_tokens: 100,
     model: "text-curie-001",
-    prompt: messageText + " (please keep your answer within 100 words)",
+    prompt: msg.text + " (please keep your answer within 100 words)",
     temperature: 0,
   });
 
   bot.sendMessage(chatId, reply.data.choices[0].text);
+});
+
+// Start the bot listening on the specified port
+bot.listen(port, () => {
+  console.log(`Bot listening on port ${port}`);
 });
